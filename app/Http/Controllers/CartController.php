0eb449c8afd692e140c39aa0178883c072f2e383
sajida-dev <sajidajaved604@gmail.com>
2026-01-08 11:40:32 +0500

    init commit

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Product $product)
+    {
+        $cart = Cart::firstOrCreate([
+            'user_id' => Auth::id(),
+        ]);
+
+        $item = $cart->items()->firstOrCreate(
+            ['product_id' => $product->id],
+            ['quantity' => 0]
+        );
+
+        $item->increment('quantity');
+
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Cart $cart)
+    {
+        //
+    }
+}
